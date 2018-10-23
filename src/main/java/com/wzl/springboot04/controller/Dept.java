38f84b3e5b4fa37761bb4e47c62db1f3cbba161a
wangzhilong <1113076904@qq.com>
2018-10-23 15:04:33 +0800

    测试合并分支

---
+++ src/main/java/com/wzl/springboot04/controller/Dept.java
@@ -0,0 +1,13 @@
+package com.wzl.springboot04.controller;
+
+public class Dept {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
